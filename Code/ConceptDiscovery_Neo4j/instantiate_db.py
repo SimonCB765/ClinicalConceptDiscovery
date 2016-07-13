1f842c8bfe158cd533fb18e7240c076290912e2b
sb0080 <s.c.bull@surrey.ac.uk>
2016-07-13 13:37:11 +0100

    Finalised the creation of the database

--- Code/ConceptDiscovery_Neo4j/instantiate_db.py
+++ Code/ConceptDiscovery_Neo4j/instantiate_db.py
@@ -1,11 +1,14 @@
 """Setup the Neo4j database."""
 
+# Python imports.
+import os
+
 # 3rd party imports.
 import neo4j.v1 as neo
 
 
 def main(databaseAddress, fileCodeDescriptions, fileHierarchy, dbUsername="neo4j", dbPassword="root", delimiter='\t'):
-    """Setup the Neo4j database from a Read code hierarchy.
+    """Setup the Neo4j database from files of code definitions and relationships.
 
     :param databaseAddress:         The address of the Neo4j database to use.
     :type databaseAddress:          str
@@ -31,39 +34,53 @@ def main(databaseAddress, fileCodeDescriptions, fileHierarchy, dbUsername="neo4j
     # overlap between code names in different hierarchies.
     # Indices need to be created on :Code.code and :Code.description as these are searched on frequently. And index does
     # not need to be created on :Word.word as this is created automatically by the addition of the unique constraint.
+    # The code format property is also indexed, however format properties are transient and this index will have
+    # minimal impact outside of node creation.
     dbExists = sum([i[0] for i in session.run("MATCH (:DBExists) RETURN 1")])
     if not dbExists:
         session.run("CREATE (:DBExists)")  # Add the dummy node indicating that the database has been created.
         session.run("CREATE CONSTRAINT ON (word:Word) ASSERT word.word IS UNIQUE")  # Each word must be unique.
         session.run("CREATE INDEX ON :Code(code)")  # Index code names.
         session.run("CREATE INDEX ON :Code(description)")  # Index code descriptions.
-
-    #TODO Set label for the type of the code from the value in the file.
-    #TODO http://stackoverflow.com/questions/24992977/neo4j-cypher-creating-nodes-and-setting-labels-with-load-csv
-    #TODO http://stackoverflow.com/questions/29419634/set-label-based-on-data-within-load-csv?noredirect=1&lq=1
+        session.run("CREATE INDEX ON :Code(format)")  # Index the code formats.
 
     # Create the codes, words and the relationships between codes and words.
     # Have to double brace {{ }} the node and edge parameters to account for the use of a formatted string.
+    # The Code.format property is set outside the MERGE in order to ensure that the merge does not try and match it.
+    # This would fail as the format gets removed later, and therefore the merge would create a duplicate node.
     query = ("USING PERIODIC COMMIT 500 "
              "LOAD CSV WITH HEADERS FROM 'file:/{0:s}' AS line FIELDTERMINATOR '\t' "
              "WITH line, split(line.words, ';') AS words "
-             "LIMIT 10 "
+             "LIMIT 5 "
              "UNWIND words AS word "
-             "MERGE (c:Code:line.format {{code: line.code, description: line.description, level: toInt(line.level)}}) "
+             "MERGE (c:Code {{code: line.code, description: line.description, level: toInt(line.level)}}) "
+             "ON CREATE SET c.format = line.format "
              "MERGE (w:Word {{word: word}}) "
-             "MERGE (c) -[d: DescribedBy]-> (w)"
+             "MERGE (c) -[d:DescribedBy]-> (w)"
              ).format(fileCodeDescriptions)
     session.run(query)
 
-    #TODO There are now (potentially) multiple labels for reach relationship, alter it so this works
+    # Update the format labels on the newly added codes. As the format property is removed once the labels are updated,
+    # this will only match those codes that are newly added.
+    session.run("MATCH (c:Code {format: 'ReadV2'}) "
+                "SET c:ReadV2 "
+                "REMOVE c.format")
+    session.run("MATCH (c:Code {format: 'CTV3'}) "
+                "SET c:CTV3 "
+                "REMOVE c.format")
+    session.run("MATCH (c:Code {format: 'SNOMED'}) "
+                "SET c:SNOMED "
+                "REMOVE c.format")
 
     # Create the relationships between parent and child nodes.
     # Have to double brace {{ }} the node and edge parameters to account for the use of a formatted string.
     query = ("USING PERIODIC COMMIT 500 "
              "LOAD CSV WITH HEADERS FROM 'file:/{0:s}' AS line FIELDTERMINATOR '\t' "
-             "WITH line "
-             "LIMIT 10 "
-             "MERGE (:Code {{code: line.parent}}) -[:Parent]-> (:Code {{code: line.child}})"
+             "WITH line, split(line.relationships, ';') AS relationships "
+             "LIMIT 5 "
+             "MATCH (child:Code {{code: line.child }}) "
+             "MATCH (parent:Code {{code: line.parent }}) "
+             "CREATE (child) -[p:Parent {{relationships: relationships}}]-> (parent)"
              ).format(fileHierarchy)
     session.run(query)
 
@@ -71,9 +88,5 @@ def main(databaseAddress, fileCodeDescriptions, fileHierarchy, dbUsername="neo4j
     session.close()
 
     # Remove the temporary files added.
-    #os.remove(fileCodesAndWords)
-    #os.remove(fileHierarchy)
-
-
-def instantiate_snomed_database():
-    pass
+    os.remove(fileCodeDescriptions)
+    os.remove(fileHierarchy)
